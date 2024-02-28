cdf098b485b97915948512b65a041107c4718e0a
Magcann <115948781+Magcann@users.noreply.github.com>
2024-02-29 00:55:07 +0300

    pressEnter()

--- src/test/java/Pages/StartPage.java
+++ src/test/java/Pages/StartPage.java
@@ -1,14 +1,18 @@
 package Pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static java.time.Duration.ofSeconds;
 
 public class StartPage {
     public StartPage() {
-        open("http://elmadev.gazprom-media.ru");
+        open("https://elmadev.gazprom-media.ru");
     }
 
     public MainPage login (){
@@ -16,16 +20,9 @@ public class StartPage {
         $(byName("authLogin")).click();
         $(byName("authLogin")).setValue("m.magnichkin");
         $(byName("password")).click();
-        $(byName("password")).setValue("Password");
-        $(By.cssSelector(".submit-button")).click();
+        $(byName("password")).setValue("Password))").pressEnter();
+        // EnterPass.shouldBe(Condition.visible).click();
         return new MainPage();
+        }
     }
 
-
-
-
-
-
-
-
-}
